2c1c37d6c8e07c12e45069b6e59d9737fb2169cc
Suzanne Combs <suzannecombs@Suzannes-MacBook-Pro.local>
2024-10-01 23:11:30 -0600

    I accidently put this into the root folder and am moving it here

---
+++ Assignments/Assignment_2/Assignment_2.R
@@ -0,0 +1,26 @@
+# Saves all the .csv files found in the Data/ directory
+csv_files <- list.files(path = "../../Data/", recursive = FALSE, pattern = ".csv")
+
+# How many .csv files found in Data/ directory
+length(csv_files)
+  
+# Read wingspan_vs_mass.csv and store contents
+df <- read.csv("../../Data/wingspan_vs_mass.csv")
+
+# Inspect the first 5 lines of df
+head(df, 5)
+  
+# Recursively find files in Data/ directory with letter "b"
+has_b <- list.files(path = "../../Data/", recursive = TRUE, pattern = "b", full.names = TRUE)
+
+# For-loop displays first line of each file in has_b
+for (i in has_b) {
+  first_line <- readLines(i, n = 1)
+  print(first_line)
+}
+
+# For-loop displays first line of each .csv file
+for (j in csv_files) {
+  first_line <- readLines(j, n = 1)
+  print(first_line)
+}
